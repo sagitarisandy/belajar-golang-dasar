055b9aebf163305062a5f7916f9310bf41276102
sagitarisandy <97603275+sagitarisandy@users.noreply.github.com>
2026-01-28 14:56:50 +0700

    Push Again

--- belajar-golang-standard-library/regexp.go
+++ belajar-golang-standard-library/regexp.go
@@ -11,4 +11,6 @@ func main() {
 	fmt.Println(regex.MatchString("eko"))
 	fmt.Println(regex.MatchString("edo"))
 	fmt.Println(regex.MatchString("eKo"))
+
+	fmt.Println(regex.FindAllString("eko edo egi ego e1o eto eKo", 10))
 }
